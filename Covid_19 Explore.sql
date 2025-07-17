f001c3c6c4c9b41e996b1fcc32b6803275e5f021
Kaushal Dahiwale <93283262+kaushaldahiwale@users.noreply.github.com>
2025-07-17 18:04:57 +0530

    Format Update Covid_19 Explore.sql

--- Covid_19 Explore.sql
+++ Covid_19 Explore.sql
@@ -1,114 +1,164 @@
 --select data that we are going to be using
-
-select location,date,total_cases,new_cases,total_deaths,population
-from coviddeaths_19
-where continent is not null
-order by 1,2;
+SELECT location,
+       date,
+       total_cases,
+       new_cases,
+       total_deaths,
+       population
+FROM   coviddeaths_19
+WHERE  continent IS NOT NULL
+ORDER  BY 1,
+          2;
 
 --Looking at Total Cases vs Total Deaths
 -- show likelihood of dying in india
-select location,date,total_cases,total_deaths,(total_deaths/total_cases) * 100 as DeathPercentage 
-from coviddeaths_19
-where location like '%india%'
-order by 1,2
-;
+SELECT location,
+       date,
+       total_cases,
+       total_deaths,
+       ( total_deaths / total_cases ) * 100 AS DeathPercentage
+FROM   coviddeaths_19
+WHERE  location LIKE '%india%'
+ORDER  BY 1,
+          2;
+
 --Looking at total Cases vs Total Population
-select location,date,total_cases,(total_cases/population) * 100 as InfectionPercentage 
-from coviddeaths_19
+SELECT location,
+       date,
+       total_cases,
+       ( total_cases / population ) * 100 AS InfectionPercentage
+FROM   coviddeaths_19
 --where location like '%india%'
-order by 1,2;
+ORDER  BY 1,
+          2;
 
 --Looking at countries with highest infection rate compared to population
-select location,max(total_cases) as highestInfectionCount,Max((total_cases/population)) * 100 as InfectionPercentage 
-from coviddeaths_19
+SELECT location,
+       Max(total_cases)                        AS highestInfectionCount,
+       Max(( total_cases / population )) * 100 AS InfectionPercentage
+FROM   coviddeaths_19
 --where location like '%india%'
-group by location,population
-order by InfectionPercentage desc;
+GROUP  BY location,
+          population
+ORDER  BY infectionpercentage DESC;
 
 --Showing countries with highest death count per population
-select location,max(convert(int,total_deaths))  as maxDeathCount
+SELECT location,
+       Max(CONVERT(INT, total_deaths)) AS maxDeathCount
 --where location like '%india%'
-from coviddeaths_19
-where continent is not null
-group by location
-order by maxDeathCount desc;
+FROM   coviddeaths_19
+WHERE  continent IS NOT NULL
+GROUP  BY location
+ORDER  BY maxdeathcount DESC;
 
 -- by continent
-
-select location,max(convert(int,total_deaths))  as maxDeathCount
-from coviddeaths_19
-where continent is  null
-group by location
-order by maxDeathCount desc;
+SELECT location,
+       Max(CONVERT(INT, total_deaths)) AS maxDeathCount
+FROM   coviddeaths_19
+WHERE  continent IS NULL
+GROUP  BY location
+ORDER  BY maxdeathcount DESC;
 
 --global number
-select date,sum(new_cases) as total_cases, sum(convert (int,new_deaths)) as total_death
-,sum(convert (int,new_deaths))/sum(new_cases) *100 as DeathPercentage
-from CovidDeaths_19
-where continent is not null 
-group by date
-order by 1,2;
+SELECT date,
+       Sum(new_cases)                                        AS total_cases,
+       Sum(CONVERT (INT, new_deaths))                        AS total_death,
+       Sum(CONVERT (INT, new_deaths)) / Sum(new_cases) * 100 AS DeathPercentage
+FROM   coviddeaths_19
+WHERE  continent IS NOT NULL
+GROUP  BY date
+ORDER  BY 1,
+          2;
 
 --join
 -- total population vs total vaccination
-select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
-,sum(convert(int,vac.new_vaccinations)) over (partition by dea.location order by dea.date) as RollingPeopleVaccinated
-from CovidDeaths_19 dea
-join CovidVaccinations_19 vac
-on dea.location = vac.location and dea.date = vac.date
-where dea.continent is not null
-order by 2,3;
+SELECT dea.continent,
+       dea.location,
+       dea.date,
+       dea.population,
+       vac.new_vaccinations,
+       Sum(CONVERT(INT, vac.new_vaccinations))
+         OVER (
+           partition BY dea.location
+           ORDER BY dea.date) AS RollingPeopleVaccinated
+FROM   coviddeaths_19 dea
+       JOIN covidvaccinations_19 vac
+         ON dea.location = vac.location
+            AND dea.date = vac.date
+WHERE  dea.continent IS NOT NULL
+ORDER  BY 2,
+          3;
 
 --use cte
-with PopvsVac (continent,location,date,population,new_vaccinations,RollingPeopleVaccinated)
-as 
-(
-select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
-,sum(convert(int,vac.new_vaccinations)) 
-over (partition by dea.location order by dea.location,dea.date) as RollingPeopleVaccinated
-from CovidDeaths_19 dea
-join CovidVaccinations_19 vac
-on dea.location = vac.location and dea.date = vac.date
-where dea.continent is not null
---order by 2,3
-)
-select *,(RollingPeopleVaccinated/population)*100 
-from PopvsVac
-
+WITH popvsvac (continent, location, date, population, new_vaccinations,
+     rollingpeoplevaccinated)
+     AS (SELECT dea.continent,
+                dea.location,
+                dea.date,
+                dea.population,
+                vac.new_vaccinations,
+                Sum(CONVERT(INT, vac.new_vaccinations))
+                  OVER (
+                    partition BY dea.location
+                    ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+         FROM   coviddeaths_19 dea
+                JOIN covidvaccinations_19 vac
+                  ON dea.location = vac.location
+                     AND dea.date = vac.date
+         WHERE  dea.continent IS NOT NULL
+        --order by 2,3
+        )
+SELECT *,
+       ( rollingpeoplevaccinated / population ) * 100
+FROM   popvsvac
 
 --Temp Table
-create table #percentPopulationVaccinated
-(
-Continent nvarchar (255),
-Location nvarchar (255),
-Date dateTime,
-population numeric,
-New_vaccinations numeric,
-RollingPeopleVaccinated numeric
-)
+CREATE TABLE #percentpopulationvaccinated
+  (
+     continent               NVARCHAR (255),
+     location                NVARCHAR (255),
+     date                    DATETIME,
+     population              NUMERIC,
+     new_vaccinations        NUMERIC,
+     rollingpeoplevaccinated NUMERIC
+  )
 
-insert into #percentPopulationVaccinated
+INSERT INTO #percentpopulationvaccinated
+SELECT dea.continent,
+       dea.location,
+       dea.date,
+       dea.population,
+       vac.new_vaccinations,
+       Sum(CONVERT(INT, vac.new_vaccinations))
+         OVER (
+           partition BY dea.location
+           ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+FROM   coviddeaths_19 dea
+       JOIN covidvaccinations_19 vac
+         ON dea.location = vac.location
+            AND dea.date = vac.date
+WHERE  dea.continent IS NOT NULL
 
-select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
-,sum(convert(int,vac.new_vaccinations)) 
-over (partition by dea.location order by dea.location,dea.date) as RollingPeopleVaccinated
-from CovidDeaths_19 dea
-join CovidVaccinations_19 vac
-on dea.location = vac.location and dea.date = vac.date
-where dea.continent is not null
 --order by 2,3
-
-select *,(RollingPeopleVaccinated/population)*100 
-from #percentPopulationVaccinated
+SELECT *,
+       ( rollingpeoplevaccinated / population ) * 100
+FROM   #percentpopulationvaccinated
 
 --create view
-create view  percentPopulationVaccinated as 
-select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations
-,sum(convert(int,vac.new_vaccinations)) 
-over (partition by dea.location order by dea.location,dea.date) as RollingPeopleVaccinated
-from CovidDeaths_19 dea
-join CovidVaccinations_19 vac
-on dea.location = vac.location and dea.date = vac.date
-where dea.continent is not null
+CREATE VIEW percentpopulationvaccinated
+AS
+  SELECT dea.continent,
+         dea.location,
+         dea.date,
+         dea.population,
+         vac.new_vaccinations,
+         Sum(CONVERT(INT, vac.new_vaccinations))
+           OVER (
+             partition BY dea.location
+             ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+  FROM   coviddeaths_19 dea
+         JOIN covidvaccinations_19 vac
+           ON dea.location = vac.location
+              AND dea.date = vac.date
+  WHERE  dea.continent IS NOT NULL
 --order by 2,3
-
